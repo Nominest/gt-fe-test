5b0403bdd9610521e6e6a7f5691d8a0cffdc6415
nominest <nominest3@gmail.com>
2023-04-13 11:49:40 +0800

    first init

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Main from "./Main";
+
+type PropType = {
+  children: ReactNode;
+};
+export default function Layout({ children }: PropType) {
+  return (
+    <div>
+      <Header />
+      <div className="grid md:grid-cols-sidebar">
+        <Sidebar />
+      </div>
+
+      <Main />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Sidebar() {
+  return (
+    <div>
+      <Link href={"/user"}>user</Link>
+    </div>
+  );
+}
